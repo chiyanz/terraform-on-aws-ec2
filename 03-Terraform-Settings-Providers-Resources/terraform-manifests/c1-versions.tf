1a0347b175e32309f722ff298ca880df55a0f485
chiyanz <jonathanqyz@gmail.com>
2023-05-27 16:40:37 -0400

    update version control arguments and ec2 ami

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -1,16 +1,18 @@
 # Terraform Block
 terraform {
-  required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = "~> 1.4" # allows version 1.4.xx, 1.5,xx and above. Does not support 2.0 and above
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"
     }
-  } 
-}  
+  }
+}
+
 # Provider Block
 provider "aws" {
   region = "us-east-1"
+  # profile = "default" atomatically chosen
 }
 
 /*
